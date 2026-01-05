fc544f02627bbb90715b7294455dad42b6fc856a
vsheo <vireshsheoratan@gmail.com>
2026-01-05 14:13:38 +0100

    static svelte site

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<h1>Welcome to a SvelteKit static site</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+
+
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXTFRwk4pvnHBw0bD7D-G5hr2PmiCEkxYuXw&s" alt="mekanik">
